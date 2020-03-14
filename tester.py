22cfa79303379d8838ed2ebe4bd64ed2aabe61ab
alex-ong <the.onga@gmail.com>
2020-03-15 08:57:45 +1100

    Initial commit

---
+++ tester.py
@@ -0,0 +1,20 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import time
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('authentication.json', scope)
+with open("sheetinfo.json") as f:    
+    data = json.load(f)
+    sheetName = data["sheetID"]
+gc = gspread.authorize(credentials)
+
+
+wks = gc.open_by_key(sheetName)
+ccEditor = wks.worksheet('CTMCC Results')
+
+t = time.time()
+cell_list = ccEditor.range('B2:G30')
+
+print (time.time() - t)    
